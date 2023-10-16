e428e4bd2e3051e6cb2e87dc84a347e8a35f5b67
Rana Umar Shakoor <120050041+RanaUmarShakoor@users.noreply.github.com>
2023-10-16 23:56:31 +0500

    Carousel Made

--- js/custom.js
+++ js/custom.js
@@ -43,3 +43,36 @@ $("#dropdownPricingButton").on("click", function(){
     }
 })
 
+
+
+  $(document).ready(function(){
+    $(".owl-carousel").owlCarousel();
+  });
+
+  $('.owl-carousel').owlCarousel({
+    loop:false,
+    margin:10,
+    nav:true,
+    responsiveClass: true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    },
+    navText: [
+        `<svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 36 36" fill="none">
+        <circle cx="18" cy="18" r="17.5" fill="white" stroke="#D9D9D9"/>
+        <path d="M17.024 18.0006L21.1488 22.1253L19.9703 23.3038L14.667 18.0006L19.9703 12.6973L21.1488 13.8758L17.024 18.0006Z" fill="#11174C"/>
+        </svg>`,
+        `<svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 36 36" fill="none class="nextBtn" style="transform: rotate(180deg);">
+        <circle cx="18" cy="18" r="17.5" fill="white" stroke="#D9D9D9"/>
+        <path d="M17.024 18.0006L21.1488 22.1253L19.9703 23.3038L14.667 18.0006L19.9703 12.6973L21.1488 13.8758L17.024 18.0006Z" fill="#11174C"/>
+        </svg>`,
+      ],
+})
